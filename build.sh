b26cd9adfaae0791dec1cfc665da8c668bcaaead
hesthatguy1 <jordan.savior15@gmail.com>
2023-08-08 01:55:58 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+export ARCH=arm64 && export SUBARCH=arm64
+
+# PATH=${PATH}:~/Android/toolchains/AOSP/Clang/clang-r383902c/bin
+# export CLANG_TRIPLE=aarch64-linux-gnu-
+
+export PATH=${PATH}:~/Android/toolchains/AOSP/GCC/aarch64-linux-android-4.9/bin
+export CROSS_COMPILE=aarch64-linux-android-
+
+export PATH=${PATH}:~/Android/toolchains/AOSP/GCC/arm-linux-androideabi-4.9/bin
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+make clean && make mrproper
+make O=out clean && make O=out mrproper
+make O=out wahoo_defconfig
+# make -j$(nproc --all) O=out CC=clang
+make -j$(nproc --all) O=out
